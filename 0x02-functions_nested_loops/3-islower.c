8fff0f341c4c98a84d8fc7c29463d814a1ef0aff
Oduola Fawaz <oduolafawaz@gmail.com>
2022-03-15 16:20:06 +0000

    check lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  _islower - entry point
+ *
+ *  Description: check for lower case
+ *
+ *  Return 0
+ */
+
+int _islower(int c)
+{
+	int letters;
+	
+	for (letters = 'a'; letters <= z; letters++)
+	{
+		if (letters == c)
+		{
+			return (1);
+		}
+	}
+	return (0);	
+}
