aaa13d9a96450e2067524cc5546979ea85cf0173
Oduola Fawaz <oduolafawaz@gmail.com>
2022-03-15 15:59:49 +0000

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Description - print 10 the alphabet
+ * in lower case followed by a new line
+ *
+ * Return void
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int i=0, i <= 10, i++)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_!putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
