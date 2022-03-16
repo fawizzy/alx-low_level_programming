44e831905f1557569a1caa5b04a0f6a220c2e847
Oduola Fawaz <oduolafawaz@gmail.com>
2022-03-16 21:35:52 +0000

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print to 98
+ * @n: first number
+ * Description: print from n to 98
+ * return: 0;
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf(n);
+		n++;
+	}
+}
