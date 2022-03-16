77a60f5626dd564dd7d41fe0fa9046c5e505c522
Oduola Fawaz <oduolafawaz@gmail.com>
2022-03-16 02:36:03 +0000

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - check the sign
+ * Description: print the sign of a number
+ * @c: character to be tested
+ * Return: 1 if c is greater than zero,
+ * 0 if c is zero and -1 if less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > o)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+
+}
