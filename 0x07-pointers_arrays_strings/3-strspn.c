6a5856c904144bea382458bb787c893419f0ac29
Fawaz Oduola <oduolafawaz@gmail.com>
2022-03-29 23:07:18 +0000

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,12 +16,16 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				len++;
 				break;
+			}
+			else if (accept[j + 1] == '\0')
+			{
+				return (len);
 			}
 		}
 	}
