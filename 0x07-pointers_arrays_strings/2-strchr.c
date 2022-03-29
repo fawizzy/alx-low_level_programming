b0c54fe863aaf70279dc311a37e1c235795ee358
Fawaz Oduola <oduolafawaz@gmail.com>
2022-03-29 19:47:19 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strchr - returns a pointer to the first occurence 
+ * of a character
+ * @s: set of characters
+ * @c: the character to check
+ * Return: pointer to the first occurence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
