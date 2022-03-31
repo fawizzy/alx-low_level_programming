b08988c0e8dbf1877f3ef6f2cd548c1a9e58aba0
Fawaz Oduola <oduolafawaz@gmail.com>
2022-03-31 13:59:56 +0000

    error corrected

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,21 +3,28 @@
 /**
  * _sqrt_recursion - find square root
  * @n: the number whose sqrt is to be found
- * Return square root of n
+ * Return: square root of n
  */
 
 int _sqrt_recursion(int n)
 {
 	int root = n;
-	return check_root(n,root);
+
+	return (check_root(n, root));
 }
+/**
+ * check_root - find square root
+ * @x: the number whose sqrt is to be found
+ * @root: guessed root
+ * Return: square root of n
+ */
 
 int check_root(int x, int root)
 {
-	if (root * root == x)
-		return root;
+	if ((root * root) == x)
+		return (root);
 	else if (root == 0 || x < 0)
 		return (-1);
 	else
-		return check_root(x, root - 1);
+		return (check_root(x, root - 1));
 }
