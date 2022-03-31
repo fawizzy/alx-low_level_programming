e92cc0f38ecafc54956c84b03a6b95e97c643740
Fawaz Oduola <oduolafawaz@gmail.com>
2022-03-31 14:55:40 +0000

    check prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,10 +15,10 @@ int is_prime_number(int n)
 
 	if (n == 0 || n == 1)
 		return (0);
-	else if (x == 2)
+	else if (n == 2)
 		return (1);
 	else
-		return check_prime(x, factor + 1);
+		return check_prime(n, factor + 1);
 }
 
 int check_prime(int x, int factor)
