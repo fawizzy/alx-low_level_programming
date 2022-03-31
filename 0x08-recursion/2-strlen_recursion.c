a9bb80a182ddf256103feefcf25c7f6e563eaf50
Fawaz Oduola <oduolafawaz@gmail.com>
2022-03-31 02:25:29 +0000

    print length of string recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return the length of string
+ * @s: the string to find the length
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else 
+		return (0);
+}
