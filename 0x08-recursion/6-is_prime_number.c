e2909a61a16b24bb3dce6073f27b97556dfd5ff0
Fawaz Oduola <oduolafawaz@gmail.com>
2022-03-31 15:06:08 +0000

    check prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -21,6 +21,13 @@ int is_prime_number(int n)
 		return (check_prime(n, factor + 1));
 }
 
+/**
+ * check_prime - check prime numbers
+ * @x: number to check
+ * @factor: check if its divisible by factor
+ * Return: 1 if prime
+ * 0 if not prime
+ */
 int check_prime(int x, int factor)
 {
 	if (x % factor == 0 && x != factor && factor > 1)
