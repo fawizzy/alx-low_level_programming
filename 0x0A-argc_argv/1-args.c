77dccc7f8660fdb508bfaeeee7690c5374dceb53
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-02 22:46:49 +0000

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: lnumber of command line argument
+ * @argv: array of the command line argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+
+	return (0);
+}
