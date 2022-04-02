26640c48d9ace58fb3e3bc66a26c6afd9984f7bc
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-02 22:29:01 +0000

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry function
+ * @argc: number of command line arguments
+ * @argv: array of the command line arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
