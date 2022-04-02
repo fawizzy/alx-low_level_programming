1cac3b1a815c10f98ac87d64aa4ff29ac2edb416
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-02 23:51:56 +0000

    print all argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments it receives
+ * @argc: number of arguments received
+ * @argv: array of arguments received
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
