c906562b06b66831bccbb0b34a11b25484d00264
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-04 09:05:48 +0000

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,17 +11,17 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, product;
-
+	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	product = num1 * num2;
-	if (argc != 3)
-	{
-		printf("error\n");
-		return (1);
-	}
-	else
-		printf("%d\n", product);
+
+	printf("%d\n", product);
 
 	return (0);
 }
