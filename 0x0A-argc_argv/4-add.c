b88450b2296922ebf20a41d6080bc6d81b4db474
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-04 19:12:13 +0000

    sum

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two numbers
+ * @argc: number of argument
+ * @argv: array of argumment
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		int i, digit;
+
+		for (i = 1; i < argc; i++)
+		{
+			for (digit = 0; argv[i][digit]; digit++)
+			{
+				if (argv[i][digit] < '0' || argv[i][digit] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
