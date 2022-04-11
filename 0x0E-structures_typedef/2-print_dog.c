111304c16e0ad427627a741803302a4a3ad06c4a
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-11 20:51:35 +0000

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,6 +17,6 @@ void print_dog(struct dog *d)
 
 		if (d->owner == NULL)
 			d->owner = "nil";
-		printf("Name: %s\nAge: %6f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }
