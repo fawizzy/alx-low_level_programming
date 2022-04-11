aee21b10dff85b6d82b7f5cc7bc38385b0d83161
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-11 20:32:10 +0000

    dog init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - a function that initialize variable type dog
+ * @d: dog to be initialized
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ * Return: void
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d -> name = name;
+		d -> age = age;
+		d -> owner = owner;
+	}
+}
