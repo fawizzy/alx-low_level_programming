2f3a4cefd67436dbb5d0763c13b69556d4f86e14
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-12 19:16:49 +0000

    new dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,10 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			d->name = "nil";
+			d->name = "(nil)";
 
 		if (d->owner == NULL)
-			d->owner = "nil";
-		printf("Name: (%s)\nAge: %f\nOwner: (%s)\n", d->name, d->age, d->owner);
+			d->owner = "(nil)";
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }
