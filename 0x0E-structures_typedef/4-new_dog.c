1fe6967e6a4cd1319f56770f791931440c148030
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-12 19:10:53 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,7 +8,6 @@ dog_t *new_dog(char *name, float age, char *owner);
 /**
  * _strlen - Finds the length of a string.
  * @str: The string to be measured.
- * 
  * Return: The length of the string.
  */
 int _strlen(char *str)
@@ -27,7 +26,6 @@ int _strlen(char *str)
  * terminating null byte, to a buffer pointed to by dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
- * 
  * Return: The pointer to dest.
  */
 
