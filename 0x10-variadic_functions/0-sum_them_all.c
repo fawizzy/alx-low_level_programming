c09d890e23e17a5440c189959b56c375ec713550
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-15 12:44:58 +0000

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(nums, n);
 
-	for (index = 0; index < n: index++)
+	for (index = 0; index < n; index++)
 		sum += va_arg(nums, int);
 
 	va_end(nums);
