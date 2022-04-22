ef0757e7fac45cb8fe2b2dc089b309fd40871b33
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-22 14:28:43 +0000

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,15 +12,12 @@ size_t print_list(list_t *head)
 {
 	size_t count = 0;
 
-	if (head == NULL)
-	{
-		printf("list is empty");
-		return (NULL);
-	}
-
 	while (head != NULL)
 	{
-		printf("[%d] %s\n", head->len, head->str);
+		if (head->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", head->len, head->str);
 		head = head->next;
 		count++;
 	}
