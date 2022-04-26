118743e4f714497dd30b7355bdcf82ea98078679
Fawaz Oduola <oduolafawaz@gmail.com>
2022-04-26 16:47:40 +0000

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,31 +2,33 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *temp, *copy;
-    int count = 0;
+	listint_t *temp, *copy;
+	unsigned int count = 0;
 
-    copy = *head;
+	copy = *head;
 
-    if (copy == NULL)
-        return (-1);
+	if (copy == NULL)
+		return (-1);
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        free(copy);
-        return (1);
-    }
-    while (copy)
-    {
-        if (count == index - 1)
-        {
-            temp = copy->next;
-            copy->next = temp->next;
-            free(temp);
-            return (1);
-        }
-        copy = copy->next;
-        count++;
-    }
-    return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(copy);
+		return (1);
+	}
+
+	while (copy)
+	{
+		if (count == index - 1)
+		{
+			temp = copy->next;
+			copy->next = temp->next;
+			free(temp);
+			return (1);
+		}
+
+		copy = copy->next;
+		count++;
+	}
+	return (-1);
 }
