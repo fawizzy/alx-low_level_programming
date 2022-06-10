d85f4cc82285d55f42fa8f6e80bd5c0c6a59cd67
Fawaz Oduola <oduolafawaz@gmail.com>
2022-06-10 08:13:02 +0000

    task 6 done

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum of all data in  a linked list
+ * @head: first node of the lined list
+ * Return: sum of all data n n linked list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
