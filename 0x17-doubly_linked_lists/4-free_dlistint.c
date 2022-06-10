f010f523a8791bdab04393a848c2a437aba44395
Fawaz Oduola <oduolafawaz@gmail.com>
2022-06-10 05:02:31 +0000

    task 5 done

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free double linked list
+ * @head: beginning of list
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next;
+	while (head)
+	{
+		next = (head)->next;
+		free(head);
+		head = next;
+	}
+}
---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index- get node at nth index
+ * @head: beginning of list
+ * @index: position to get node from
+ * Return: node at index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int n = 0;
+
+	if (head == NULL)
+		return (NULL);
+	
+	while (head)
+	{
+		if (n == index)
+			return (head);
+		head = head->next;
+		n++;
+	}
+	return (NULL);
+}
