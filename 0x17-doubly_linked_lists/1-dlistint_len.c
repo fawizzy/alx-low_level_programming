03cb31087afe486dc94ecada08670272f2232fa1
Fawaz Oduola <oduolafawaz@gmail.com>
2022-06-10 03:51:43 +0000

    task 1 done

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len: returns the number of elements in a linked list
+ * @h: head of the linked list
+ * Return: number of element in lnked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h= h->next;
+	}
+	return (n);
+}
